c931b30aa8e4dfe94edeba103e9e3597cd959f81
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-04-19 11:17:27 +0100

    updating angulare/core

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME="udagrameedydev2";
+export POSTGRES_PASSWORD="udagrameedydev2"; 
+export POSTGRES_HOST="udagrameedydev2.cc3cvlw4gdcp.us-east-1.rds.amazonaws.com";
+export POSTGRES_DB="udagrameedydev2";
+export AWS_BUCKET="udagram-eedy-dev";
+export AWS_REGION="us-east-1";
+export AWS_PROFILE="arn:aws:iam::501925613140:user/udagram-dev";
+export JWT_SECRET="fajkafogoaoaslhfjejkwfweffhjgfsdewsafafagfqafqf";
+export URL=http://localhost:8100
