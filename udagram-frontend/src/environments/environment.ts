9b57ba3db678812dded053fcc076fe0dc8aa867b
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-04-30 19:36:49 +0100

    reset environment.ts to localhost

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://a41bd1107d0404581b42bbb47ae047ff-477145096.us-east-1.elb.amazonaws.com:8080/api/v0'
+  apiHost: 'http://localhost:8080/api/v0'
 };
 
 /*
