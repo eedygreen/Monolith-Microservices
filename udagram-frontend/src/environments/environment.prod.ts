0d3df7fe1a8bb535dc83c71274c9494b2efe8e0c
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-04-30 05:53:14 +0100

    frontend environment apiHost set to public ip

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acdd4dfe584af4ba0a9d917af47526cb-1923087734.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
