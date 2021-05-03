ef640f49b3d9b0aaa8abb1a03771427fb7aa2c67
eedygreen <idris.isah2@st.futminna.edu.ng>
2021-05-03 15:30:03 +0100

    environments

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://acc205541bcce45fdaff9e9ceb25e6ab-2123657451.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
